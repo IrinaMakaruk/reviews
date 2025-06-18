8eafdf6a42d7247a5693c7138db5a9f814e7b145
IrinaMakaruk <irunamakaruk@gmail.com>
2025-06-17 18:53:56 -0600

    fix(api): support production start on Render

--- apps/rss-fetcher-lambda/src/services/push-to-api.ts
+++ apps/rss-fetcher-lambda/src/services/push-to-api.ts
@@ -1,10 +1,10 @@
 import type { Review } from "@shared/models";
 
 export async function pushReviewsToApi(reviews: Review[]): Promise<any> {
-  const url = process.env.API_URL!;
+  const url = process.env.API_URL! ?? "http://localhost:3001";
   const apiKey = process.env.API_KEY!;
 
-  const res = await fetch(url, {
+  const res = await fetch(`${url}/api/reviews/save`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
